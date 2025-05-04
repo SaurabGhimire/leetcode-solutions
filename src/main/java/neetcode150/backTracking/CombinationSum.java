159779eefde2317cfc33552f6ff557c50490c00a
Saurab Ghimire <sourrab@gmail.com>
2025-05-04 17:28:36 -0500

    backtracking : combination sum

---
+++ src/main/java/neetcode150/backTracking/CombinationSum.java
@@ -0,0 +1,28 @@
+
+
+public class CombinationSum {
+    public static void main(String[] args) {
+        // candidates = [2,3,6,7], target = 7
+        int[] candidates = {2,3,6,7};
+        combinationSum(candidates, 7);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(0, target, candidates, subset, list); 
+        return list;
+    }
+
+    public void dfs(int i, int target, int[] candidates, List<Integer> subset, List<List<Integer>> list){
+        if(i>= candidates.length || target<0) return;
+        if(target == 0){
+            list.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(candidates[i]);
+        dfs(i, target - candidates[i], candidates, subset, list );
+        subset.remove(subset.size() - 1);
+        dfs(i+1, target, candidates, subset, list);
+    }
+}
