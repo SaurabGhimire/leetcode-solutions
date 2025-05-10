235b8a873d20f77ad0b9d9919482e70c420515d4
Saurab Ghimire <sourrab@gmail.com>
2025-05-10 10:09:19 -0500

    dynamic programming: climbing stairs bottom up

--- src/main/java/neetcode150/dynamicProgramming1d/ClimbingStairs.java
+++ src/main/java/neetcode150/dynamicProgramming1d/ClimbingStairs.java
@@ -18,3 +18,16 @@ public class ClimbingStairs {
         return numOfWays;
     }
 }
+
+class ClimbingStairsBottomUp {
+    public int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+        for(int i=0;i<n-1;i++){
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
+}
