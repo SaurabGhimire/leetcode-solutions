f76796579dcc718d1d0a24f676d54442eedcb686
Saurab Ghimire <sourrab@gmail.com>
2025-05-10 13:40:03 -0500

    dynamic programming : house robber with memoization

--- src/main/java/neetcode150/dynamicProgramming1d/HouseRobber.java
+++ src/main/java/neetcode150/dynamicProgramming1d/HouseRobber.java
@@ -11,3 +11,22 @@ public class HouseRobber {
         return Math.max(temp[temp.length-1], temp[temp.length-2]);
     }
 }
+
+class Solution {
+    public int rob(int[] nums) {
+        int[] cache = new int[nums.length];
+        for(int i=0;i<cache.length;i++){
+            cache[i]= Integer.MIN_VALUE;
+        }
+        int first = dfs(0, nums, cache);
+        int second = dfs(1, nums, cache);
+        return Math.max(first, second);
+    }
+
+    public int dfs(int i,int[] nums, int[] cache){
+        if(i>=nums.length) return 0;
+        if(cache[i]>Integer.MIN_VALUE) return cache[i];
+        cache[i] = nums[i] + Math.max(dfs(i+2,  nums, cache), dfs(i+3, nums, cache));
+        return cache[i];
+    }
+}
