50a6ef63cf615debbdbad283b41b3d51e8a450ad
Saurab Ghimire <sourrab@gmail.com>
2025-05-12 17:58:43 -0500

    dp : palindromic substrings using dp

--- src/main/java/neetcode150/dynamicProgramming1d/PalindromicSubstrings.java
+++ src/main/java/neetcode150/dynamicProgramming1d/PalindromicSubstrings.java
@@ -22,4 +22,22 @@ public class PalindromicSubstrings {
         }
         return count;
     }
+
+    class Solution {
+        public int countSubstrings(String s) {
+            boolean[][] dp = new boolean[s.length()][s.length()];
+            int count=0;
+            for(int i=s.length()-1;i>=0;i--){
+                for(int j=i;j<s.length();j++){
+                    if(s.charAt(i)==s.charAt(j) && (j-i<=2 || dp[i+1][j-1])){
+                        dp[i][j] = true;
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
 }
+
+
