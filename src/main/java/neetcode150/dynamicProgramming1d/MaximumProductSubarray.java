5a4d88eb1d0979ff75d105c9862cfe25ea326f8b
Saurab Ghimire <sourrab@gmail.com>
2025-05-13 14:18:08 -0500

    dp : maximum product subarray using max min

---
+++ src/main/java/neetcode150/dynamicProgramming1d/MaximumProductSubarray.java
@@ -0,0 +1,20 @@
+package neetcode150.dynamicProgramming1d;
+
+public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int largestProduct = nums[0];
+        for(int i=1;i<nums.length;i++){
+            int curr = nums[i];
+            int p1 = max[i-1] * curr;
+            int p2 = min[i-1] * curr;
+            max[i] = Math.max(curr, Math.max(max[i - 1] * curr, min[i - 1] * curr));
+            min[i] = Math.min(curr, Math.min(max[i - 1] * curr, min[i - 1] * curr));
+            largestProduct = Math.max(max[i], largestProduct);
+        }
+        return largestProduct;
+    }
+}
