1170d1a9508276f6e980bee3405c2d1f1f05c838
Saurab Ghimire <sourrab@gmail.com>
2025-05-12 19:32:17 -0500

    dp : decode ways using bottom up approach

--- src/main/java/neetcode150/dynamicProgramming1d/DecodeWays.java
+++ src/main/java/neetcode150/dynamicProgramming1d/DecodeWays.java
@@ -22,4 +22,26 @@ public class DecodeWays {
         map.put(i, count);
         return count;
     }
+
+    // Bottom up approach 
+    class Solution {
+        public int numDecodings(String s) {
+            int n = s.length();
+            int[] dp = new int[n+1];
+            dp[n] = 1;
+            dp[n-1] = s.charAt(s.length()-1) == '0' ? 0 : 1;
+            for(int i=n-2;i>=0;i--){
+                if(s.charAt(i) == '0') {
+                    dp[i] = 0;
+                    continue;
+                }
+                dp[i] = dp[i+1];
+                int val = Integer.valueOf(s.substring(i, i+2));
+                if(val>9 && val<=26){
+                    dp[i] += dp[i+2];
+                }
+            }
+            return dp[0];
+        }
+    }   
 }
