e0ca3309db451c431a58901fe0b9d249c92476df
Saurab Ghimire <sourrab@gmail.com>
2025-05-19 16:14:29 -0500

    greedy: jump game ii using greedy

--- src/main/java/neetcode150/greedy/JumpGameII.java
+++ src/main/java/neetcode150/greedy/JumpGameII.java
@@ -1,30 +1,20 @@
 package neetcode150.greedy;
 
 public class JumpGameII {
-    public int jump(int[] nums) {
-        Queue<int[]> queue = new LinkedList<>(); //index, count
-        queue.add(new int[]{0, 0});
-        int count = 0;
-        boolean[] visited = new boolean[nums.length];
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            for(int i=0;i<size;i++){
-                int[] item = queue.remove();
-                int index = item[0];
-                count = item[1]; // 0
-                int num = nums[index];  // 2
-                for(int j=1;j<=num;j++){
-                    int nextIndex = index+j;
-                    if(nextIndex >= nums.length) break;
-                    if(nextIndex == nums.length-1){
-                        return count+1;
-                    }
-                    if(visited[nextIndex]) continue;
-                    visited[nextIndex] = true;
-                    queue.add(new int[]{nextIndex, count+1});
-                }
+    
+     public int jump(int[] nums) {
+        int jumps = 0, left = 0, right = 0;
+
+        while (right < nums.length - 1) {
+            int farthest = 0;
+            for (int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
             }
+            left = right + 1;
+            right = farthest;
+            jumps++;
         }
-        return count;
+
+        return jumps;
     }
 }
