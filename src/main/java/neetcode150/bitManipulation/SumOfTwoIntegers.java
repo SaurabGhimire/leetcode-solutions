4f3eba481c53dd5fdafc3698de8ce37e471f65b1
Saurab Ghimire <sourrab@gmail.com>
2025-05-21 17:24:45 -0500

    bit manipulation: sum of two integers

---
+++ src/main/java/neetcode150/bitManipulation/SumOfTwoIntegers.java
@@ -0,0 +1,12 @@
+package neetcode150.bitManipulation;
+
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int temp = a;
+            a = a^b;
+            b = (temp & b) << 1;
+        }
+        return a;
+    }
+}
